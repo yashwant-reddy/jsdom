4f573ef526824a2f3c81511aad727a8b1fb49ce0
yashwant <a.yashwantreddy31@gmail.com>
2021-01-09 20:36:35 +0530

    second commit

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 
 //console.dir(document);
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-console.log(document.head);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.head);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all.textContent='hello';
+
