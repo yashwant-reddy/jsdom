d14477962be49aa57de65e7ee523b15cf732621b
yashwant <a.yashwantreddy31@gmail.com>
2021-01-09 21:55:24 +0530

    third commit

--- test.js
+++ test.js
@@ -14,10 +14,33 @@
 
 //selectors for queries in dom
 var headerTitle=document.getElementById("header-title");
+var header=document.getElementById("main-header");
 // headerTitle.textContent="pitti";
 // headerTitle.innerText="itti";
 
 // console.log(headerTitle.textContent);
 // console.log(headerTitle.innerText);
+
+// headerTitle.innerHTML='<h3>hello</h3>;
+// headerTitle.style.borderBottom='solid 3px black';
+// header.style.borderBottom='solid 3px black';
+
+//getting elemnts by class name//
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+items[1].textContent='hell';
+items[1].style.fontWeight='bold';
+//gives error
+// items.style.backgroundColor='#f4f4f4';
+
+
+for (var i =0;i<items.length;i++){
+    items[i].style.backgroundColor='#f4f4f4';
+}
+
+//get elemnts bt tag name
+
+
+
 
 
