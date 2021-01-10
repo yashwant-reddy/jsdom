0771353094526b4210b69576d0c7c78185af3f00
yashwant <a.yashwantreddy31@gmail.com>
2021-01-10 20:54:21 +0530

    seventh commit

--- test.js
+++ test.js
@@ -76,6 +76,12 @@ var titles=document.querySelectorAll('.title');
 console.log(titles);
 titles[0].textContent='helllo';
 
+var odd=document.querySelectorAll('li:nth-child(odd)');
+var even=document.querySelectorAll('li:nth-child(even)');
 
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor='#f4f4f4';
+    even[i].style.backgroundColor='#f1f111';
+}
 
 
